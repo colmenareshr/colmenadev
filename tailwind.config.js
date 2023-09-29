7e2392e49e9f0c69b68710c50879de8f7ce223ad
Humberto Colmenares <colmenareshr@gmail.com>
2023-09-28 23:35:29 -0300

    adicionando estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,22 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: '#f4f4f4',
+      gray: '#c9d1d9',
+      primaryBackground: '#000000',
+      secondaryBackground: '#333333',
+      primaryText: '#EEEEEE',
+      blue: '#0070f3',
+      black: '#0a0a0a',
+      yellow: '#ffbd2e',
+      orange: '#fc6d26',
+      purple: '#6b4fbb',
+      green: '#27c93f',
+      fucsia: '#de1d8d',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
     },
   },
   plugins: [],
