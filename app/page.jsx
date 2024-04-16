db0a972ce1b74a71fa2bf3841869f3b66be892e6
Humberto Colmenares <colmenareshr@gmail.com>
2024-04-15 22:56:28 -0300

    improve the hero section on desktop version

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,24 @@
 import SocialProfile from '@/components/SocialProfile';
-import { FaSignature } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <main className='min-h-screen'>
-      <section className='container mx-auto flex flex-col space-y-3 p-8'>
-        <h1 className='text-balance text-left'>Humberto Colmenares </h1>
-        <h2 className='text-lg text-slate-200'>
-          Desarrollador Frontend y especialista en Wordpress
-        </h2>
-        <p>
-          Te ayudo a desarrollar aplicaciones y sitios web modernos, SEO
-          optimizados y adaptados a dispositivos móviles.
-        </p>
-        <SocialProfile />
-      </section>
-      <section className='min-h-96 flex items-center justify-center container mx-auto bg-slate-200 p-8 my-photo__bg'>
-        <figure className='my-photo'>
-        </figure>
-      </section>
+    <main className='md:min-h-screen max-w-full'>
+      <div className='container max-w-full bg-slate-900 md:min-h-screen flex flex-col md:flex-row md:items-center hero-bg px-8'>
+        <section className='container mx-auto md:w-3/4 flex flex-col space-y-3 p-8'>
+          <h1 className='text-balance text-left'>Humberto Colmenares </h1>
+          <h2 className='text-lg text-slate-200'>
+            Desarrollador Frontend y especialista en Wordpress
+          </h2>
+          <p className='text-balance'>
+            Te ayudo a desarrollar aplicaciones y sitios web modernos, SEO
+            optimizados y adaptados a dispositivos móviles.
+          </p>
+          <SocialProfile />
+        </section>
+        <section className='max-w-full md:min-h-screen flex items-center justify-center container mx-auto p-8 my-photo__bg'>
+          <figure className='my-photo'></figure>
+        </section>
+      </div>
     </main>
   );
 }
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,13 +1,17 @@
+'use client';
+import Link from 'next/link';
 import { CiMenuFries } from 'react-icons/ci';
 
 const Navbar = () => {
   return (
-    <nav className='max-w-full mx-auto px-8 py-4'>
+    <nav className='max-w-full mx-auto px-16 py-4 bg-slate-800 shadow-lg'>
       <div className='flex items-center justify-between flex-wrap'>
         <div>
-          <span className='text-2xl text-yellow-400 font-extrabold'>
-            ColmenaDev
-          </span>
+          <Link href={'/'}>
+            <span className='text-2xl text-yellow-400 font-extrabold md:text-4xl'>
+              ColmenaDev
+            </span>
+          </Link>
         </div>
         <div>
           <CiMenuFries size={40} color='#FFD163' />
